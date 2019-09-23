22aca210ed89aab736c8bc4bc7156d4e956de4f2
Dima <dima@mail.com>
2019-09-23 10:23:26 +0300

    add first

---
+++ class_db.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+class db:
+
+    def __init__(self):
+        self.nameBD = 'base.db'
+
+    def connectDB(self):
+        self.conn = sqlite3.connect(self.nameBD)
+        self.cur = self.conn.cursor()
+
+    def disconnect(self):
+        self.cur.close()
+        self.conn.close()
+
+    def query(self, query, varAggr = False):
+        if varAggr:
+            return self.cur.execute(query, varAggr)
+        else:
+            return self.cur.execute(query)
+
+    def creatRecord(self, listAttr):
+        self.connectDB()
+        self.result = self.query('INSERT INTO `records` VALUES (NULL, ?, ?, ?, ?)', listAttr)
+        self.conn.commit()
+        self.disconnect()
+
+    def selectRecord(self, columnRecord = False, agrRecord = False):
+        self.connectDB()
+        if columnRecord != '*' and agrRecord != '*':
+            self.result = self.query('SELECT * FROM `records` WHERE {} = ?'.format(columnRecord), (agrRecord,))
+            self.data = self.result.fetchall()
+            self.disconnect()
+            return self.data
+        self.result = self.query('SELECT * FROM `records`')
+        self.data = self.result.fetchall()
+        self.disconnect()
+        return self.data
+
+    def deleteRecord(self, columnRecord, agrRecord):
+        self.connectDB()
+        self.result = self.query('DELETE FROM `records` WHERE {} = ?'.format(columnRecord), (agrRecord,))
+        self.conn.commit()
+        self.disconnect()
+
+    def updateRecord(self, columnRecord, columnRecordValue, columnRecordWho, columnRecordValueWho):
+        self.connectDB()
+        self.result = self.query('UPDATE `records` SET {} = ? WHERE {} = ?'.format(columnRecord, columnRecordWho), (columnRecordValue, columnRecordValueWho,))
+        self.conn.commit()
+        self.disconnect()
---
+++ main.py
@@ -0,0 +1,49 @@
+from class_db import *
+from colorama import init
+from colorama import Fore, Back, Style
+init()
+
+dataBase = db()
+
+newContact = list()
+
+#router
+def query(data):
+    if data == 'help':
+        return print(Fore.BLUE + "Add record: name email phone adress\n"
+                     "Select record: name/email/phone/adress={...} [or all, example: *=*]\n"
+                     "Delete record: del name/email/phone/adress={...}\n"
+                     "Update record: upd name/email/phone/adress={...} (where) name/email/phone/adress={...} [example: upd email=newemailjessi@mail.com name=Jessi]\n"
+                                 "Close note: press Space and Enter")
+    if 'del' in data:
+        listAgr = data.split('=')
+        listAgr[0] = listAgr[0][4:]
+        dataBase.deleteRecord(listAgr[0], listAgr[1])
+        return print(Fore.RED + 'Done')
+    if 'upd' in data:
+        listAgr = data.split(' ')
+        listAgr.pop(0)
+        secondListAgr = list()
+        for x in range(len(listAgr)):
+            secondListAgr.append(listAgr[x].split('='))
+        dataBase.updateRecord(secondListAgr[0][0], secondListAgr[0][1], secondListAgr[1][0], secondListAgr[1][1])
+        return print(Fore.YELLOW + 'Done')
+    if '=' in data:
+        listAgr = data.split('=')
+        result = dataBase.selectRecord(listAgr[0], listAgr[1])
+        for x in range(len(dataBase.selectRecord(listAgr[0], listAgr[1]))):
+            print(Fore.GREEN + str(result[x]))
+    else:
+        if data == ' ':
+            exit(Fore.MAGENTA + 'Close note contact')
+        countContact = data.split(' ')
+        for x in countContact:
+            newContact.append(x)
+        dataBase.creatRecord(newContact)
+        print(Fore.LIGHTGREEN_EX + 'Done')
+
+if __name__ == "__main__":
+    while True:
+        data = input()
+        query(data)
+        print(Fore.RESET + Back.RESET + Style.RESET_ALL)
